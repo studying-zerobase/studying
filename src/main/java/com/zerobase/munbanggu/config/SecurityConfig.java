2395e9fd4804810e6ad58e132a43635597554dd5
jexnjeux <zieun.na@gmail.com>
2023-10-28 08:33:07 +0900

    Feat: #3 OAuth2 설정한 SecurityConfig

--- src/main/java/com/zerobase/munbanggu/config/SecurityConfig.java
+++ src/main/java/com/zerobase/munbanggu/config/SecurityConfig.java
@@ -1,8 +1,39 @@
 package com.zerobase.munbanggu.config;
 
+import com.zerobase.munbanggu.config.auth.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint().userService(customOAuth2UserService);
+
+        return http.build();
+    }
+
 
 }
