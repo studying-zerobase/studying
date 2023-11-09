7d74abef5308ac8b3e150ebc5304f5b299611592
jexnjeux <zieun.na@gmail.com>
2023-11-09 11:21:22 +0900

    test: 투표글 생성 테스트 (#11)

---
+++ src/main/resources/sql/service-test-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER(email, password,  nickname,  phone, failed_count,created_date)
+values ('abc@gmail.com', 'password1234', 'nickname', '010-3333-4444', 0, '2023-10-29 23:01:10');
---
+++ src/test/java/com/zerobase/munbanggu/studyboard/service/StudyBoardPostServiceTest.java
@@ -0,0 +1,65 @@
+package com.zerobase.munbanggu.studyboard.service;
+
+import com.zerobase.munbanggu.studyboard.model.dto.StudyBoardPostRequest;
+import com.zerobase.munbanggu.studyboard.model.dto.StudyBoardPostResponse;
+import com.zerobase.munbanggu.studyboard.model.dto.VoteOptionRequest;
+import com.zerobase.munbanggu.studyboard.model.dto.VoteRequest;
+import com.zerobase.munbanggu.studyboard.repository.StudyBoardPostRepository;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+@SpringBootTest
+@SqlGroup({
+        @Sql(value = "/sql/service-test-data.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD),
+})
+class StudyBoardPostServiceTest {
+
+    @Autowired
+    private StudyBoardPostService studyBoardPostService;
+
+    @Autowired
+    private StudyBoardPostRepository studyBoardPostRepository;
+
+    @Test
+    void testStudyBoardPostVote() {
+
+        // given
+        List<VoteOptionRequest> optionRequests = new ArrayList<>();
+        VoteOptionRequest optionRequest = new VoteOptionRequest();
+        optionRequest.setOptionText("option1");
+        optionRequests.add(optionRequest);
+        optionRequest = new VoteOptionRequest();
+        optionRequest.setOptionText("option2");
+        optionRequests.add(optionRequest);
+
+        String dateTimeAsString = "2023-11-11T11:00:00";
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+        LocalDateTime localDateTime = java.time.LocalDateTime.parse(dateTimeAsString, formatter);
+
+        VoteRequest voteRequest = new VoteRequest("투표 제목", optionRequests, localDateTime);
+        StudyBoardPostRequest request = StudyBoardPostRequest.builder()
+                .title("게시글 제목")
+                .content("게시글 내용")
+                .vote(voteRequest)
+                .userId(1L)
+                .build();
+
+        // when
+        StudyBoardPostResponse response = studyBoardPostService.createStudyBoardPost(request);
+
+        // then
+        Assertions.assertThat(response.getTitle()).isEqualTo("게시글 제목");
+
+    }
+
+
+}
