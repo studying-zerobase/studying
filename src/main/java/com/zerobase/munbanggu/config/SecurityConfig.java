7e54cfdab1ba62f936e6cea8b7875becee7d3c70
jexnjeux <zieun.na@gmail.com>
2023-11-12 19:52:49 +0900

    feat: #34 unauthorized 처리

--- src/main/java/com/zerobase/munbanggu/config/SecurityConfig.java
+++ src/main/java/com/zerobase/munbanggu/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -40,7 +41,7 @@ public class SecurityConfig {
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+                .exceptionHandling() .defaultAuthenticationEntryPointFor(unauthorizedHandler, new AntPathRequestMatcher("/api/**"))
                 .and()
                 .authorizeHttpRequests()
                 .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/api/auth/**").permitAll()
